5d8c0005e67a08dd07ff95c9bef19610b4dc8b71
LeonorMe <leonomedeiros@gmail.com>
2022-11-17 18:56:20 +0000

    TP2_02_FormulaResolvente

--- src/TP2_08_Normalizar.java
+++ src/TP2_08_Normalizar.java
@@ -8,55 +8,39 @@ public class TP2_08_Normalizar {
         String nomeInteiro = scanner.nextLine();
 
         String[] nomes = nomeInteiro.split("\\s+/");
-        String[] excluir = {"do", "da", "dos", "das", "e", "o", "a"};
+        String[] excluir = {"do", "da", "dos", "das", "e", "a", "o"};
 
         for(String nome: nomes ){
             if(Arrays.asList(excluir).contains(nome)){
-                // se tiver maiusculas passar nome para minusculas
-                for(char letra: nome){
-                    if(ToCode(letra) < 97){
-                        letra = ToChar(ToCode(letra)+32);
-                    }
-                }
-            }else{ // é um nome normal, a primeira tem de ser mauiscula
-                for(char letra: nome) {
-                    // if primeira letra do nome:
-                    if (ToCode(letra) >= 97) { // se 1º for minuscula passar para maiuscula
-                        nome[0] = ToChar(ToCode(nome[0]) - 32);
-                    }
-                    //else as outras, passar todas para minusculas se ainda nao forem
-                    for (char letra : nome) {
-                        if(ToCode(letra) < 97){
-                            letra = ToChar(ToCode(letra)+32);
-                        }
+                // se tiver M -> m
+                for(int i=0;  i<nome.length(); i++){
+                    int letraCode = nome.charAt(i);
+                    if(letraCode < 97){
+                        //nome.charAt(i) = (char)(letraCode + 32);
+                        System.out.print((char)(letraCode + 32));
+                    }else{
+                        System.out.print(nome.charAt(i));
                     }
                 }
             }
+            else{ // é um nome normal, a primeira tem de ser M
+                for(int i=0;  i<nome.length(); i++) {
+                    char letra = nome.charAt(i);
+                    int letraCode = letra;
 
-        }
-
-
-/*
-        if(ToCode(nome[0]) >= 97 ){
-            nome[0] =ToChar(ToCode(nome[0])-32);
-        }
-        boolean maiuscula = true;
-        for(int i=1; i< nome.length(); i++){
-            if(nome[i] == " "){
-                maiuscula = true;
-            }
-            else{
-                if(ToCode(letra) >= 97 ){
-                    letra =ToChar(ToCode(letra)-32);
+                    if (i==0 && letraCode >= 97) { // se 1º for m -> M
+                        letra = (char)(letraCode - 32);
+                    }
+                    else if(letraCode < 97){
+                            letra = (char)(letraCode + 32);
+                    }
+                    //nome.charAt(i) = letra;
+                    System.out.print(letra);
                 }
-            }
-            if(maiuscula == true){
-                if(ToCode(nome[i]) >= 97 ){
-                    nome[i] =ToChar(ToCode(nome[0])-32);
-                }
-            }
 
+            }
+            System.out.print(" ");
         }
-        */
+
     }
 }
